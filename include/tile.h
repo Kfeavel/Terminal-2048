f93f08730a8565347cc1799f16d54f031ee27406
Zachary D. Kemp <zdkemp@cedarville.edu>
2019-07-27 13:49:21 -0400

    tile.h crap

---
+++ include/tile.h
@@ -0,0 +1,34 @@
+/**
+ * @file tile.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-07-27
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+class Tile {
+    public:
+        Tile();
+        ~Tile();
+        int getValue(void);
+        void setValue(int);
+        int getX(void);
+        void setX(int);
+        int getY(void);
+        void setY(int);
+    private:
+        int value;
+        int x;
+        int y;
+};
+
+Tile::Tile(void){
+    ;
+}
+
+Tile::~Tile(void){
+    ;
+}
