c92e1b2308d2046b8aa1eb927879dcba01c18f65
Keeton T. Feavel <keetonfeavel@gmail.com>
2019-07-27 14:30:20 -0400

    Updated the tile header with documentation

--- include/tile.hpp
+++ include/tile.hpp
@@ -1,6 +1,6 @@
 /**
  * @file tile.h
- * @author zach kemp
+ * @author Keeton Feavel and Zack Kemp
  * @brief tile header file with variables, etc
  * @version 0.1
  * @date 2019-07-27
@@ -11,13 +11,51 @@
 
 class Tile {
     public:
+        /**
+         * @brief Construct a new Tile object
+         * 
+         * @param v Tile value
+         * @param x X coordinate
+         * @param y Y coordinate
+         */
         Tile(int v, int x, int y);
+        /**
+         * @brief Destroy the Tile object
+         * 
+         */
         ~Tile();
+        /**
+         * @brief Get the Value object
+         * 
+         * @return int Tile value (i.e. 2, 4, 8, 16, 32, etc.)
+         */
         int getValue(void);
+        /**
+         * @brief Set the Value object
+         * 
+         */
         void setValue(int);
+        /**
+         * @brief Returns the X coordinate value
+         * 
+         * @return int X coordinate
+         */
         int getX(void);
+        /**
+         * @brief Sets the X coordinate value
+         * 
+         */
         void setX(int);
+        /**
+         * @brief Returns the Y coordinate value
+         * 
+         * @return int Y coordinate
+         */
         int getY(void);
+        /**
+         * @brief Sets the Y coordinate value
+         * 
+         */
         void setY(int);
     private:
         int value;
